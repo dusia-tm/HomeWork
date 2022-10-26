bf17525bea97405508d0ca696a66e339d8f8590e
dusia.tm <ppink@mail.ru>
2022-10-26 18:24:59 +0300

    Initial commit

---
+++ src/home_work_1/class4_5.java
@@ -0,0 +1,18 @@
+package home_work_1;
+
+import java.util.Scanner;
+
+public class class4_5 {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите букву");
+        String line = in.nextLine();
+        char c = line.charAt(0);
+        if( Character.isDigit(c))
+            System.out.println(c +" это цифра");
+        else if (Character.isLetter(c))
+            System.out.println(c +" это буква");
+    }
+
+}
