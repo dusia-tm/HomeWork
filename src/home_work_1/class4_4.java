4524591a08daca9a5161702d14965f3e27d0bf75
dusia.tm <ppink@mail.ru>
2022-10-26 08:33:25 +0300

    Initial commit

---
+++ src/home_work_1/class4_4.java
@@ -0,0 +1,22 @@
+package home_work_1;
+
+import java.util.Scanner;
+
+public class class4_4 {
+    public static void main(String[] args) {
+            System.out.println("Введите число а: ");
+            int a = new Scanner(System.in).nextInt();
+            System.out.println("Перевести в байты: B или килобайты K");
+            String zt = new Scanner(System.in).nextLine();
+
+            if (zt.length() == 1) {
+                if (zt.charAt(0) == 'B') {
+                    System.out.println("B = " + (a * 1024));
+                } else if (zt.charAt(0) == 'K') {
+                    System.out.println("K = " + (a / 1024));
+                } else {
+                    System.out.println("Неверный выбор: " + zt);
+                }
+        }
+    }
+}
